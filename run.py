b7b4960610099e6831bce440824c21e4f3b6dd9c
Zachary <1734015+nopslip@users.noreply.github.com>
2021-02-10 10:21:14 -0700

    adding gaming endpoint

--- run.py
+++ run.py
@@ -22,7 +22,24 @@ def parks_pass(pass_id):
         'description': 'ETHDenver 2021 - ColoradoJam - Parks Pass NFT',
         'image': image_url
     })
-     
+
+@nft_meta_app.route('/gaming/v/<license_id>')
+def gaming_license(license_id):
+    license_id = int(license_id)
+
+    if license_id == 1:
+        image_url = 'https://gateway.pinata.cloud/ipfs/QmSJqmXREd1f2WPV7hg9DK1fr3RQTURnGx9QqnjV1j98aW/iconfinder_colorado_state_flag_usa_5681508.png' 
+    elif license_id == 2:
+        image_url = 'https://gateway.pinata.cloud/ipfs/QmbJiWqEyJ7U9dWagyMCGATqm6MkWLtKGt3sZdDpBChHoH/colo-parks-logo-768x768.jpg' 
+    else:
+        image_url = 'https://gateway.pinata.cloud/ipfs/QmbJiWqEyJ7U9dWagyMCGATqm6MkWLtKGt3sZdDpBChHoH/colo-parks-logo-768x768.jpg'
+
+    return jsonify({
+        'name': 'Colorado Digital Gaming License',
+        'description': 'ETHDenver 2021 - ColoradoJam - Players Gaming License NFT',
+        'image': image_url
+    })
+
 
 @nft_meta_app.route('/api/get-json')
 def hello():
