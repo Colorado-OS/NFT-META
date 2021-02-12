0778feffc2112b34cc9a7459711268407c5b5330
Zachary <1734015+nopslip@users.noreply.github.com>
2021-02-12 08:04:37 -0700

    adding custom gaming nfts

--- run.py
+++ run.py
@@ -28,11 +28,11 @@ def gaming_license(license_id):
     license_id = int(license_id)
 
     if license_id == 1:
-        image_url = 'https://gateway.pinata.cloud/ipfs/QmSJqmXREd1f2WPV7hg9DK1fr3RQTURnGx9QqnjV1j98aW/iconfinder_colorado_state_flag_usa_5681508.png' 
+        image_url = 'https://gateway.pinata.cloud/ipfs/QmX6D6rtrer2BoK2YceHsdYbwDPoPugdXXYHGrLsCLsrZj/Colorado_NFT_SIDE_A.png' 
     elif license_id == 2:
-        image_url = 'https://gateway.pinata.cloud/ipfs/QmbJiWqEyJ7U9dWagyMCGATqm6MkWLtKGt3sZdDpBChHoH/colo-parks-logo-768x768.jpg' 
+        image_url = 'https://gateway.pinata.cloud/ipfs/QmQsgkLL4mB5SqqXeQaPgBh73qjAjBsYPWV9DgH1M6eBzS/Colorado_NFT_SIDE_B.png' 
     else:
-        image_url = 'https://gateway.pinata.cloud/ipfs/QmbJiWqEyJ7U9dWagyMCGATqm6MkWLtKGt3sZdDpBChHoH/colo-parks-logo-768x768.jpg'
+        image_url = 'https://gateway.pinata.cloud/ipfs/QmQsgkLL4mB5SqqXeQaPgBh73qjAjBsYPWV9DgH1M6eBzS/Colorado_NFT_SIDE_B.png'
 
     return jsonify({
         'name': 'Colorado Digital Gaming License',
